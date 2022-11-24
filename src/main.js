2f00271baad765c28e01a5efead2d0b0af615404
solvedDev <solvedDev@gmail.com>
2022-11-24 16:02:46 +0100

    v0.0.8

--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
-const { invoke } = window.__TAURI__.tauri;
+const { invoke } = window.__TAURI__.tauri
+const { listen } = window.__TAURI__.event
 
-let greetInputEl;
-let greetMsgEl;
+let greetInputEl
+let greetMsgEl
 
 async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+	// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+	greetMsgEl.textContent = await invoke('greet', { name: greetInputEl.value })
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document
-    .querySelector("#greet-button")
-    .addEventListener("click", () => greet());
-});
+window.addEventListener('DOMContentLoaded', () => {
+	greetInputEl = document.querySelector('#greet-input')
+	greetMsgEl = document.querySelector('#greet-msg')
+	document
+		.querySelector('#greet-button')
+		.addEventListener('click', () => greet())
+})
+
+listen('tauri://update-status', (res) => {
+	document.body.appendChild(document.createTextNode(JSON.stringify(res)))
+})
